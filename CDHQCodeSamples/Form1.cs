91ccad6b3d0fe572b7c74309671fe92d6fe0fdf6
Gord Todd <gord.todd@crossbydewar.com>
2018-01-05 11:30:43 -0500

    My First Commit

---
+++ CDHQCodeSamples/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CDHQCodeSamples
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string gordon = "x";
+            gordon.ToString();
+            //this is a test
+
+
+        }
+    }
+}
